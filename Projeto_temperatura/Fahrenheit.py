8cc837c17fed7b9a56d542cc3b2f44f4332001be
AnaRibeiro297 <ana.j.ferreira19@aluno.senai.br>
2025-04-08 09:08:59 -0300

    atividade somativa
    Esse teste foi alterado

--- Projeto_temperatura/Fahrenheit.py
+++ Projeto_temperatura/Fahrenheit.py
@@ -2,10 +2,14 @@
 
 
 print("Ola usuario, me de uma tempratura em Celsius e vou lhe retornar em Fahrenheit")
+def 1000/ 2
+break 
+if temp== (19+0)
     global temp, num1
     if temp == "C" or temp == "c":
         contaf = (num1 - 32) * 9/5
         print("A temperatura em Celsius é de", contaf)
     elif temp == "F":
+            def tem == "x"
             print("A temperatura em Fahrenheit é de", contac)
-
+codigo()
